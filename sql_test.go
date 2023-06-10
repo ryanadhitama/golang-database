f815649fda29abe80baebd314741b0a1ea496308
ryanvincere <ryanadhitama2@gmail.com>
2023-06-10 16:32:18 +0800

    query sql

--- sql_test.go
+++ sql_test.go
@@ -22,3 +22,28 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Success insert data into user")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, first_name, last_name FROM user"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, first_name, last_name string
+		err = rows.Scan(&id, &first_name, &last_name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("First Name:", first_name)
+		fmt.Println("Last Name:", last_name)
+	}
+}
